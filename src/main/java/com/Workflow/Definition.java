b7c9d64413307f762ef2bdf5a7f7bb0e4942840a
Nicolas Madeo <nicomad.17@gmail.com>
2022-08-26 02:47:22 -0300

    - Definition now has a Start - Definition can now return its Flow Ends - Definition can now return its Flow Length - Definition can now return its Flow Start

--- src/main/java/com/Workflow/Definition.java
+++ src/main/java/com/Workflow/Definition.java
@@ -1,13 +1,36 @@
 package com.Workflow;
 
+import java.util.List;
+
+import com.Element.FlowElement.Event.End;
+import com.Element.FlowElement.Event.Start;
+
 public class Definition {
 
     String id;
     String description;
 
-    Element
+    Start start;
 
     public Definition() {
+        this.start = new Start();
+        this.start.setNext(new End());
+    }
+
+    public Workflow createWorkflow() {
+        return new Workflow(this);
+    }
+
+    public Start getFlowStart() {
+        return this.start;
+    }
+
+    public List<End> getFlowEnds() {
+        return this.start.getEnds();
+    }
+
+    public Integer getFlowLength() {
+        return this.start.countNexts() + 1;
     }
 
 }
