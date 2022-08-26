ca78e0d2255ecab1404e403ed9576fad48f1d261
Nicolas Madeo <nicomad.17@gmail.com>
2022-08-26 02:45:45 -0300

    - End getEnds update

--- src/main/java/com/Element/FlowElement/Event/End.java
+++ src/main/java/com/Element/FlowElement/Event/End.java
@@ -1,7 +1,15 @@
 package com.Element.FlowElement.Event;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.Element.FlowElement.FlowElement;
 
 public class End extends FlowElement {
 
+    @Override
+    public List<End> getEnds() {
+        return Arrays.asList(new End[] { this });
+
+    }
 }
