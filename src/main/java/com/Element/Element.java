f545e78b264217a55df463206dc0c4757628fec6
Nicolas Madeo <nicomad.17@gmail.com>
2022-08-26 02:45:58 -0300

    - Element class updated

--- src/main/java/com/Element/Element.java
+++ src/main/java/com/Element/Element.java
@@ -1,5 +1,33 @@
 package com.Element;
 
+import java.util.List;
+
+import com.Element.FlowElement.Event.End;
+
 public abstract class Element {
+
+    protected Element next;
+
+    public Element() {
+    }
+
+    public void setNext(Element next) {
+        this.next = next;
+    }
+
+    public void addNext(Element newNext) {
+        if (this.next != null) {
+            newNext.addNext(this.next);
+            this.next = newNext;
+        } else {
+            this.next = newNext;
+        }
+    }
+
+    public abstract List<End> getEnds();
+
+    public Integer countNexts() {
+        return this.next != null ? this.next.countNexts() + 1 : 0;
+    }
 
 }
